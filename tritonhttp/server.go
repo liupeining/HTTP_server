4019c854a1b4477135410afce0730f1ee6262ad3
liupeining <pel015@ucsd.edu>
2024-04-30 16:56:06 -0700

    succefully parse the first request line

--- tritonhttp/server.go
+++ tritonhttp/server.go
@@ -1,9 +1,13 @@
 package tritonhttp
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
+	"time"
 )
 
 type Server struct {
@@ -68,16 +72,78 @@ func (s *Server) ListenAndServe() error {
 		}
 		fmt.Println("accepted connection", conn.RemoteAddr())
 		go s.HandleConnection(conn)
-		if conn != nil {
-			conn.Close()
-			break
-		}
 	}
 	return nil
 }
 
 // HandleConnection reads requests from the accepted conn and handles them.
 func (s *Server) HandleConnection(conn net.Conn) {
-	// now doing nothing
-	_ = conn.Close()
+	br := bufio.NewReader(conn) // create a new buffered reader, which reads from the connection
+	// continuously read from connection
+	for {
+		// Set timeout, if the connection is idle for 5 seconds, close the connection
+		if err := conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+			log.Printf("Failed to set timeout for connection %v", conn)
+			_ = conn.Close()
+		}
+
+		// Read request from the client
+		_, err := ReadRequest(br)
+		if err != nil {
+			log.Printf("Failed to read request from connection %v", conn)
+			_ = conn.Close()
+			return
+		}
+	}
+
+}
+
+// ReadRequest reads and parses a request from the buffered reader.
+func ReadRequest(br *bufio.Reader) (req *Request, err error) {
+	req = &Request{}
+	// Read the first line of the request, which contains the method, URL, and protocol
+	// eg. GET /index.html HTTP/1.1
+	firstLine, err := br.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error in reading first line: ", err)
+		return nil, err
+	}
+	fmt.Println("First Line: ", firstLine)
+	err = parseFirstLine(firstLine, req)
+	if err != nil {
+		fmt.Println("Error in parsing first line: ", err)
+		return nil, err
+	}
+	// check req
+	fmt.Println("Request: ", req)
+	return req, nil
+}
+
+func parseFirstLine(firstLine string, req *Request) error {
+	parts := strings.Split(firstLine, " ")
+	if len(parts) != 3 {
+		return fmt.Errorf("invalid first line: %q", firstLine)
+	}
+	// check if the method is valid (only GET is supported)
+	if parts[0] != "GET" {
+		return fmt.Errorf("invalid method: %q", parts[0])
+	}
+	req.Method = parts[0]
+	fmt.Println("Method: ", req.Method)
+
+	// check if the URL is valid
+	if !strings.HasPrefix(parts[1], "/") {
+		return fmt.Errorf("invalid URL: %q", parts[1])
+	}
+	req.URL = parts[1]
+	fmt.Println("URL: ", req.URL)
+
+	// check if the protocol is valid
+	protocol := strings.TrimSpace(parts[2])
+	if protocol != "HTTP/1.1" {
+		return fmt.Errorf("invalid protocol: %q", parts[2])
+	}
+	req.Proto = protocol
+	fmt.Println("Protocol: ", req.Proto)
+	return nil
 }
